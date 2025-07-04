2863ad20b102fe9178b1a7ccf3644bb9f6af0b66
Shanks522 <shankaran.srivatsa@gmail.com>
2025-07-04 12:14:52 -0700

    Removed Last Valid Races

--- main.py
+++ main.py
@@ -8,53 +8,6 @@ from sklearn.metrics import accuracy_score
 import fastf1
 
 class F1Predictor:
-    def get_last_valid_races(driver_code,year,num_races=3):
-        schedule = fastf1.get_event_schedule(year,include_testing=False)
-        recent_valid_races = []
-        number=5
-        
-        for num in range(1,len(schedule)):
-            event = schedule.loc[schedule['RoundNumber']==len(schedule)-num]
-            eventName = event['EventName'].values[0]
-            eventDate = event['Session1Date']
-            event_date_value = pd.to_datetime(event['EventDate'].values[0]).tz_localize(None)
-            if event_date_value>datetime.now():
-                print("Didn't Happen did it")
-                continue
-            print("EVENT NAME ISSSS", eventName)
-            try:
-                race = fastf1.get_session(year,eventName,'R')
-                race.load(telemetry = False, weather= False, laps=False,messages=False)
-                results = race.results.query(f"Abbreviation == '{driver_code.upper()}' ")
-                if not results.empty:
-                    status = results.iloc[0]['Status']
-                    if 'Finished' in status or 'Lap' in status:
-                        gridpos=results.iloc[0]['GridPosition']
-                        pos=results.iloc[0]['Position']
-                        recent_valid_races.append(eventName)
-                        recent_valid_races.append(gridpos)
-                        recent_valid_races.append(pos)
-                        
-                        
-                        if len(recent_valid_races)>=9:
-                            number = num
-                            break
-
-                else:
-                    print("RESULT EMPTYYYYYY")
-                    continue
-            except:
-                continue
-        try:
-            event= schedule.loc[schedule['RoundNumber']==len(schedule)-number+3]
-            eventName = event['EventName'].values[0]
-            recent_valid_races.append(eventName)
-        except IndexError:
-            track4="Unknown"
-            recent_valid_races.append(track4)
-        columns = ['Track1','Grid1','Finish1','Track2','Grid2','Finish2','Track3','Grid3','Finish3','Track4']
-        return pd.DataFrame([recent_valid_races],columns=columns)
-    
       
     def generate_training_data(driver_code,year,num_races=3):
         training_data=[]
@@ -149,18 +102,12 @@ for col in ['Finish1', 'Finish2', 'Finish3']:
 
 Xtrain= training_data.drop(columns=['TargetFinish'])
 Ytrain= training_data['TargetFinish']
-#XTest = F1Predictor.get_last_valid_races('Ha',2025)
 tracks = ['Track1','Track2','Track3','Track4','Finish1','Finish2','Finish3']
 Xtrain[tracks] = Xtrain[tracks].fillna('Unknown')
-#XTest[tracks] = XTest[tracks].fillna('Unknown')
 Xtrain = pd.get_dummies(Xtrain, columns=tracks)
 
-print("YOOOOO LOOK AT ME")
-#print(XTest)
-#XTest = XTest.dropna()
+
 model = RandomForestClassifier()
-#if XTest.isnull().values.any():
-    #print("NULL VALUES")
 if Xtrain.isnull().values.any():
     print("NULL VALUESSSSS")
 print(f"Xtrain shape: {Xtrain.shape}")
@@ -171,8 +118,6 @@ model.fit(Xtrains,Ytrains)
 Ypred = model.predict(X_val)
 accuracy = accuracy_score(Ypred,y_val)
 print("Training Accuracy:",accuracy)
-#YTest= model.predict(XTest)
-#print("Predicted Position:", YTest) 
             
         
 
